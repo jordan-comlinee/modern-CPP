a6877567c392d22a483e809c864a62eaf17e1f62
jordan-comlinee <leesoyoung0112@gmail.com>
2022-09-20 14:11:10 +0900

    2022-09-20

--- ch03/ch03/main.cpp
+++ ch03/ch03/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// 포인터 사용 응용, 주소에 의한 호출
 void swap(int* x, int* y) {
 	int temp;
 	temp = *x;
@@ -10,12 +11,27 @@ void swap(int* x, int* y) {
 
 }
 
+// 참조에 의한 호출의 경우...
+void swap(int& x, int& y) {
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+	cout << x << '\t' << y << endl;
+
+}
+
 
 int main() {
 	int n_a = 10;
 	int n_b = 20;
 	cout << n_a << '\t' << n_b << endl;
+	// 주소에 의한 호출
 	swap(&n_a, &n_b);
-
+	// 참조에 의한 호출
+	swap(n_a, n_b);
 	cout << n_a << '\t' << n_b << endl;
 }
+
+
+//https://jhnyang.tistory.com/319 를 보면서 따로 공부해보아도 좋을 것 같다.
